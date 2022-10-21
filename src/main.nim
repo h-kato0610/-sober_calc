1149c89605478d63060cb9a859733764f5a71440
h-kato0610 <h-kato0610@gmail.com>
2022-10-22 03:21:55 +0900

    update Sober args

--- src/main.nim
+++ src/main.nim
@@ -1,14 +1,21 @@
+import os
 import times
 
+const dateFormat: string = "yyyy-MM-dd"
+var firstSober: string = ""
 
-let dateFormat: string = "yyyy-MM-dd"
-let myFirstSober: string = "2022-08-13"
+# 最初のSoberを引数で設定する.
+# 引数が設定されていなければ自分のSoberで計算する
+if (paramCount() == 0):
+    # 自分の最初のSober
+    firstSober = "2022-08-13"
+else:
+    firstSober = commandLineParams()[0]
 
-# FIXME 08-13 to args
-let firstSober = parse(myFirstSober, dateFormat)
+let firstSoberDate = parse(firstSober, dateFormat)
 let nowDate = now()
 
-echo "FirstSober : ", firstSober
+echo "FirstSober : ", firstSoberDate
 echo "NowDate : ", nowDate
 # 時差対策で+1日する
-echo "Sober history : ", (nowDate - firstSober).inDays + 1
+echo "Sober history : ", (nowDate - firstSoberDate).inDays + 1
